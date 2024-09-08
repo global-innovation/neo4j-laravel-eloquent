62c7ac09c28b806597d8edf55967e31a87733771
Matias Perrone <github@matiasperrone.com>
2024-09-08 10:13:53 -0300

    Fix usage populateStub

--- src/Migrations/MigrationCreator.php
+++ src/Migrations/MigrationCreator.php
@@ -6,26 +6,6 @@ use Illuminate\Database\Migrations\MigrationCreator as IlluminateMigrationCreato
 
 class MigrationCreator extends IlluminateMigrationCreator
 {
-    
-    protected $name = null;
-    
-    /**
-     * Create a new migration at the given path.
-     *
-     * @param  string  $name
-     * @param  string  $path
-     * @param  string|null  $table
-     * @param  bool  $create
-     * @return string
-     *
-     * @throws \Exception
-     */
-    public function create($name, $path, $table = null, $create = false)
-    {
-        $this->name = $name;
-        return parent::create($name, $path, $table = null, $create = false);
-    }
-
     /**
      * Populate the place-holders in the migration stub.
      *
@@ -36,13 +16,14 @@ class MigrationCreator extends IlluminateMigrationCreator
      */
     protected function populateStub($stub, $label)
     {
-        $stub = str_replace('{{class}}', studly_case($name), $stub);
-
         // Here we will replace the label place-holders with the label specified by
         // the developer, which is useful for quickly creating a labels creation
         // or update migration from the console instead of typing it manually.
-        if (!is_null($label)) {
-            $stub = str_replace('{{label}}', $label, $stub);
+        if (! is_null($label)) {
+            $stub = str_replace(
+                ['{{ label }}', '{{label}}'],
+                $label, $stub
+            );
         }
 
         return $stub;
